db48ab4e39a15b5bc1ad02e6970a9e555ba1adb8
A.M <andares@outlook.com>
2019-09-17 17:01:16 +0800

    update

---
+++ yuprc/ops.zsh
@@ -0,0 +1,24 @@
+
+setopt EXTENDED_GLOB
+
+export YHOME="{{yhome}}"
+export PATH="$YHOME/.bin:$PATH"
+
+_addons=(
+    https://github.com/LXTechnic/yup-tool.git
+    git@codehub.devcloud.huaweicloud.com:LXTech/yup-lx.git
+)
+
+source $YHOME/yup.zsh
+source $YHOME/.autoload
+
+ZSH_THEME=lx
+yup oh-my-style https://raw.githubusercontent.com/LXTechnic/public/master/zsh-themes/lx.zsh-theme
+
+# for freebsd console
+bindkey "\e[H" beginning-of-line
+bindkey "\e[F" end-of-line
+
+# for deno
+export DENO_INSTALL="$HOME/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
